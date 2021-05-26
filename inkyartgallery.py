09f095c5cb45567a85636ad0cecd9e568152c8f2
DennisMilek <dennis.milek@outlook.de>
2021-05-26 13:01:41 +0200

    Fixed smk attribute check

--- inkyartgallery.py
+++ inkyartgallery.py
@@ -4,20 +4,10 @@ import meetmuseum_gallery
 import random
 
 galleries = []
-#galleries.append(smk_gallery)
-galleries.append(meetmuseum_gallery)
+galleries.append(smk_gallery)
+#galleries.append(meetmuseum_gallery)
 gallery = galleries[random.randint(0,len(galleries)-1)]
-
-print(gallery)
 
 imagelist = gallery.getAllImageIds()
 imageInfo = gallery.selectRandomImage(imagelist)
 print(gallery.downloadImage(imageInfo))
-
-#imagelist = smk_gallery.getAllImageIds()
-#imageInfo = smk_gallery.selectRandomImage(imagelist)
-#print(smk_gallery.downloadImage(imageInfo))
-
-#imagelist = meetmuseum_gallery.getAllImageIds()
-#imageInfo = meetmuseum_gallery.selectRandomImage(imagelist)
-#print(meetmuseum_gallery.downloadImage(imageInfo))
--- smk_gallery.py
+++ smk_gallery.py
@@ -8,12 +8,16 @@ def getAllImageIds():
 
 
 def selectRandomImage(imageList):
-    imageID = imageList[random.randint(0, len(imageList)-1)]
-    result = requests.get('https://api.smk.dk/api/v1/art/?object_number={objectNumber}&lang=en'.format(objectNumber=imageID)).content
-    if(json.loads(result)['items'][0]['image_native'] is None):
-        selectRandomImage(imageList)
-    else:
-        return json.loads(result)
+    resultimage = None
+    while True:
+        imageID = imageList[random.randint(0, len(imageList)-1)]
+        result = requests.get('https://api.smk.dk/api/v1/art/?object_number={objectNumber}&lang=en'.format(objectNumber=imageID)).content
+
+        if(json.loads(result)['items'][0]['has_image'] == True):
+            resultimage = json.loads(result)
+            break
+
+    return resultimage
 
 
 def downloadImage(imageInfo):
