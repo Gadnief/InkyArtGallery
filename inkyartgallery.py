268ec45fd0b8c684a35dce0a6c27044caca8d35d
DennisMilek <dennis.milek@outlook.de>
2021-06-09 19:00:42 +0200

    fix

--- inkyartgallery.py
+++ inkyartgallery.py
@@ -31,7 +31,7 @@ contrastedImage = contrast.enhance(2)
 
 # Draw url
 draw = ImageDraw.Draw(contrastedImage)
-draw.multiline_text((1, 1), reflowed, fill=board.BLACK, font=ImageFont.truetype(SourceSansProSemibold, 24), align="left")
+draw.multiline_text((1, 1), "Demotext", fill=board.BLACK, font=ImageFont.truetype(SourceSansProSemibold, 24), align="left")
 
 print('Image loaded')
 print('URL: ' + imageurl)
