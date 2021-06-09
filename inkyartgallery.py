05d2e0f0ce8f3a9b4e6459f7d1a0d1ff3ec60e73
DennisMilek <dennis.milek@outlook.de>
2021-06-09 18:22:40 +0200

    Fix

--- inkyartgallery.py
+++ inkyartgallery.py
@@ -15,32 +15,6 @@ gallery = galleries[random.randint(0,len(galleries)-1)]
 imagelist = gallery.getAllImageIds()
 imageInfo = gallery.selectRandomImage(imagelist)
 
-urllib.request.urlretrieve(gallery.downloadImage(imageInfo), "gfg.png")
-
-board = Inky()
-print('board setup')
-image = Image.open("gfg.png").resize((600,448))
-print('Image loaded')
-board.set_image(image)
-print('Try to show')
-board.show()
-print('done!')import smk_gallery
-import meetmuseum_gallery
-from inky.inky_uc8159 import Inky
-from PIL import Image
-import urllib.request
-
-import random
-
-print('starting')
-galleries = []
-galleries.append(smk_gallery)
-#galleries.append(meetmuseum_gallery)
-gallery = galleries[random.randint(0,len(galleries)-1)]
-
-imagelist = gallery.getAllImageIds()
-imageInfo = gallery.selectRandomImage(imagelist)
-
 imageurl = gallery.downloadImage(imageInfo)
 urllib.request.urlretrieve(imageurl, "gfg.png")
 
