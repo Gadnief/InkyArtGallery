81c403a69baf66f5eb2d8f75ffb884443d14846c
Dennis Milek <dennis.milek@outlook.de>
2022-04-18 18:16:46 +0200

    Fixed ratio retry

--- inkyartgallery.py
+++ inkyartgallery.py
@@ -5,36 +5,50 @@ from PIL import Image, ImageOps, ImageEnhance, ImageDraw, ImageFont
 import urllib.request
 import random
 
-from font_source_sans_pro import SourceSansProSemibold
+title = ""
 
 def downloadValidImage():
-    print('Downloading image')
-    galleries = []
-    #galleries.append(smk_gallery)
-    galleries.append(meetmuseum_gallery)
-    gallery = galleries[random.randint(0, len(galleries) - 1)]
+    while True:
+        ratio = 1
+        try:
+            print('Downloading image')
+            galleries = []
+            # galleries.append(smk_gallery)
+            galleries.append(meetmuseum_gallery)
+            gallery = galleries[random.randint(0, len(galleries) - 1)]
 
-    imagelist = gallery.getAllImageIds()
-    imageInfo = gallery.selectRandomImage(imagelist)
+            imagelist = gallery.getAllImageIds()
+            imageInfo = gallery.selectRandomImage(imagelist)
 
-    downlaodedimage = gallery.downloadImage(imageInfo)
-    url = downlaodedimage['url']
-    print('URL: ' + url)
-    title = downlaodedimage['title']
-    print('Title: ' + title)
-    urllib.request.urlretrieve(url, "gfg.png")
+            downlaodedimage = gallery.downloadImage(imageInfo)
+            url = downlaodedimage['url']
+            print('URL: ' + url)
+            title = downlaodedimage['title']
+            print('Title: ' + title)
+            urllib.request.urlretrieve(url, "gfg.png")
 
-    rawimage = Image.open("gfg.png")
-    ratio = (rawimage.width / rawimage.height)
+            rawimage = Image.open("gfg.png")
+            ratio = (rawimage.width / rawimage.height)
+        except:
+            print ('Error getting image! Retry!')
 
-    return (rawimage, title)
+
+        if ratio < 0.75 and ratio > 0.6:
+            print ('Found Image!')
+            return (rawimage, title)
+        else:
+            print ('Invalid image ratio! Retry!')
+
+
 
 print('starting')
 
 board = Inky()
 print('board setup')
 
-rawimage = downloadValidImage()[0]
+imageinfo = downloadValidImage()
+rawimage = imageinfo[0]
+title = imageinfo[1]
 
 print('Transposing image')
 transposedImage = rawimage.transpose(Image.TRANSPOSE).resize((600,448))
@@ -44,13 +58,10 @@ filteredImage = filter.enhance(1.5)
 contrast = ImageEnhance.Contrast(filteredImage)
 contrastedImage = contrast.enhance(1.5)
 
-# Draw url
-draw = ImageDraw.Draw(contrastedImage)
-#draw.multiline_text((1, 1), rawimage[1], fill=board.WHITE, font=ImageFont.truetype(SourceSansProSemibold, 24), align="left")
-
 print('Image loaded')
 board.set_image(contrastedImage)
+
 print('Try to show')
 board.show()
-print('done!')
+print('DONE!')
 
